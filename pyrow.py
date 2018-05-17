ae5b386825639c5d6f52b9c6ede5169a126411bb
carey136 <robert.carey.90@gmail.com>
2018-05-17 12:48:58 +0100

    Corrected path syntax

--- pyrow.py
+++ pyrow.py
@@ -6,15 +6,15 @@ from qgis.core import *
 
 #### These are the only variable that need to be changed ####
 #### The remaining code referenced these definitions     ####
-myProject = 'C:\Temp\py\PYROW_3.qgs'
+myProject = 'C:\\Temp\\py\\PYROW_3.qgs'
 layoutName = 'A4_Atlas'
 atlasFilter = '\"parishcode\" = 207'
 exportFormat = 'image'
 imageOutputName = '\"PC_name\" || \' \' || \"NUMBER_\"'
 imageDPI = 200
 imageExtension = '.jpg'
-pdfOutputPath = 'C:/Temp/py/output/CyclicalSurvey.pdf'
-imageOutputFolder = 'C:/Temp/py/output/'
+pdfOutputPath = 'C:\\Temp\\py\\output\\CyclicalSurvey.pdf'
+imageOutputFolder = 'C:\\Temp\\py\\output\\'
 
 #### Defining map path and contents ####
 QgsProject.instance().read(myProject)
