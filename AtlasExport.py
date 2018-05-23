4a78f414502be87841a07e5998e8911c44a777f2
carey136 <robert.carey.90@gmail.com>
2018-05-23 17:23:08 +0100

    Added arguments, rewrote code

--- AtlasExport.py
+++ AtlasExport.py
@@ -7,17 +7,52 @@ from qgis.core import *
 from PyQt5.QtCore import QFileInfo
 from PyQt5.QtWidgets import QApplication
 
-#### These are the only variable that need to be changed ####
-#### The remaining code referenced these definitions     ####
+####      Hardcode variables here      ####
+#### Or use flags to override defaults ####
+
+
+# -P = Project file
 myProject = 'c:your\\project\\location.qgs'
+
+# -L = Layout ("Layout name")
 layoutName = 'atlas_name'
-atlasFilter = 'filter = query' # Use '' if no filter needed, otherwise e.g. '\"FeatureType\" = \'building\''
-exportFormat = 'image' #'image' or 'pdf'
-imageOutputName = '@atlas_featurenumber' #e.g.\"Parish\" || \' \' || \"Number\" where parish and number are columns - outputs "Newbiggin 207"
-imageDPI = 200
-imageExtension = '.jpg' # (or .png)
-pdfOutputPath = 'c:your\\output\\location.pdf'
-imageOutputFolder = 'c:your\\project\\folder\\'
+
+# -C = Coverage Layer Name ("Layer Name")
+coverageLayer = 'Layer_name'
+
+# -F = filter ("column" "operator" "value")
+atlasFilter = '"column" = "value"'
+
+# -O = Output Format (pdf or image)
+outputFormat = 'pdf'
+
+# -D = Output Directory ("c:your\output" - exclude trailing "\")
+outputFolder = 'c:your\project\folder'
+
+# -N = Image output Query
+outputName = '@atlas_featurenumber' #e.g.\"Parish\" || \' \' || \"Number\" where parish and number are columns
+
+# -Q = PDF Name
+pdfName = 'Export'
+
+
+#### Setting variables using flags ####
+if("-P" in sys.argv):
+    myProject = sys.argv[sys.argv.index("-P") + 1]
+if("-L" in sys.argv):
+    layoutName = sys.argv[sys.argv.index("-L") + 1]
+if("-C" in sys.argv):
+    coverageLayer = sys.argv[sys.argv.index("-C") + 1]
+if("-F" in sys.argv):
+    atlasFilter = "\"" + sys.argv[sys.argv.index("-F") + 1] + "\" " + sys.argv[sys.argv.index("-F") + 2] + " \'" + sys.argv[sys.argv.index("-F") + 3] + "\'"
+if("-O" in sys.argv):
+    outputFormat = sys.argv[sys.argv.index("-O") + 1]
+if("-D" in sys.argv):
+    outputFolder = sys.argv[sys.argv.index("-D") + 1]
+if("-N" in sys.argv):
+    outputName = sys.argv[sys.argv.index("-N") + 1]
+if("-Q" in sys.argv):
+    pdfName = sys.argv[sys.argv.index("-Q") + 1]
 
 #### Initialising QGIS in back end (utilising users temp folder) ####
 home = expanduser("~")
@@ -26,7 +61,6 @@ QgsApplication.setPrefixPath("C:/OSGeo4W64/apps/qgis", True) #Change path for st
 app = QApplication([])
 QgsApplication.initQgis()
 
-
 #### Defining map path and contents ####
 QgsProject.instance().read(myProject)
 myLayout = QgsProject.instance().layoutManager().layoutByName(layoutName)
@@ -34,29 +68,31 @@ myAtlas = myLayout.atlas()
 myAtlasMap = myAtlas.layout()
 
 #### atlas query ####
+if(coverageLayer in locals()):
+    myAtlas.setCoverageLayer(QgsProject.instance().mapLayersByName(coverageLayer))
 myAtlas.setFilterFeatures(True) 
 myAtlas.setFilterExpression(atlasFilter)
 
 #### image output name ####
-myAtlas.setFilenameExpression( imageOutputName )
+myAtlas.setFilenameExpression( outputName )
 
 #### image and pdf settings ####
 pdf_settings=QgsLayoutExporter(myAtlasMap).PdfExportSettings()
 image_settings = QgsLayoutExporter(myAtlasMap).ImageExportSettings()
-image_settings.dpi = imageDPI
-
+image_settings.dpi = 200
+imageExtension = '.jpg'
 #### Export images or PDF (depending on flag) ####
-
-if exportFormat == "image":
+if outputFormat == "image":
     for myLayout in QgsProject.instance().layoutManager().printLayouts():
         if myAtlas.enabled():
             result, error = QgsLayoutExporter.exportToImage(myAtlas, 
-                                baseFilePath=imageOutputFolder, extension=imageExtension, settings=image_settings)
+                                baseFilePath=outputFolder + '\\', extension=imageExtension, settings=image_settings)
             if not result == QgsLayoutExporter.Success:
                 print(error)
-if exportFormat == "pdf":
+if outputFormat == "pdf":
     for myLayout in QgsProject.instance().layoutManager().printLayouts():
         if myAtlas.enabled():
-            result, error = QgsLayoutExporter.exportToPdf(myAtlas, pdfOutputPath, settings=pdf_settings)
+            result, error = QgsLayoutExporter.exportToPdf(myAtlas, outputFolder + '\\' + pdfName + '.pdf', settings=pdf_settings)
             if not result == QgsLayoutExporter.Success:
                 print(error)
+print("Success!")
